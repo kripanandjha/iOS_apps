196e7c488a4d177b7c83754fd4362c2553d344c6
kripanandjha <jha.kripanand@gmail.com>
2018-05-27 01:46:56 -0700

    adding I am rich app

---
+++ I_am_rich/I_am_rich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I_am_rich
+//
+//  Created by project on 5/27/18.
+//  Copyright © 2018 Kripanand Jha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
